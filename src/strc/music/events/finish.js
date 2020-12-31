87b6af1e090f7f0f4bb3a2ab0c9f3bf58ce8b27e
weberww0 <60146862+weberww0@users.noreply.github.com>
2020-12-31 16:53:16 -0400

    Delete finish.js

--- src/strc/music/events/finish.js
+++
@@ -1,8 +0,0 @@
-module.exports = (player) => {
-    if(player.manager.voiceChannel.members.filter(m => !m.user.bot).size === 0) {
-        player.stop()
-        return player.manager._firstRequest.textChannel.send("Como ninguém estava conectado na chamada, eu finalizei a reprodução.")
-    }
-
-    player.skip()
-}
