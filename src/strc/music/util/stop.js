b40a23bd36b8724056fbf48d1a4be113bbd1b570
weberww0 <60146862+weberww0@users.noreply.github.com>
2020-12-31 16:53:31 -0400

    Delete stop.js

--- src/strc/music/util/stop.js
+++
@@ -1,8 +0,0 @@
-module.exports = (player, desposito) => {
-    player.dispatcher.dispatcher.destroy()
-    player.manager.voiceChannel.leave()
-    player.guild.player = null
-    desposito.players.delete(player.guild.id)
-
-    return true
-}
