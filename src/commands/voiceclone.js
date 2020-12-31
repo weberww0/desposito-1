91f79f42cdee783b8d1da91003d7e579042dc1b3
weberww0 <60146862+weberww0@users.noreply.github.com>
2020-12-31 18:27:07 -0400

    Update voiceclone.js

--- src/commands/voiceclone.js
+++ src/commands/voiceclone.js
@@ -15,15 +15,19 @@ module.exports = {
         const connection2 = await vc2.join()
 
         const message = await msg.channel.send(vc1.name + " & " + vc2.name + " conectados.\n")
-        
-        connection1.on('speaking', async (user, speaking) => {
-console.log("^^^^^^")
-            const receiver = await connection1.receiver.createStream(user.id)
+        connection1.members.forEach(m => {      
+            const receiver = await connection1.receiver.createStream(m.user.id)
             connection2.play(receiver, { type: 'opus' })
-
-            console.log(message.content + "\n" + user.tag + " está falando.")
-            receiver.on('end', () => {
-            })
         })
+
+        // connection1.on('speaking', async (user, speaking) => {
+        // console.log("^^^^^^")
+        //    const receiver = await connection1.receiver.createStream(user.id)
+        //   connection2.play(receiver, { type: 'opus' })
+        //
+        //    console.log(message.content + "\n" + user.tag + " está falando.")
+        //    receiver.on('end', () => {
+        //    })
+        //})
     }
 }
