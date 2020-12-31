9f8dff4a190a7931ade68a535af696310844db1a
weberww0 <60146862+weberww0@users.noreply.github.com>
2020-12-31 18:46:58 -0400

    Create countgame.js

---
+++ src/commands/countgame.js
@@ -0,0 +1,17 @@
+module.exports = {
+    runHelp(data, desposito) {
+
+const filter = m => !m.author.bot
+const coletor = data.message.channel.createMessageCollector(filter, {time: 60000})
+let n = 1
+
+coletor.on("collect", (col) => {
+    if(col.content == n) { n += 1 
+        col.react("👍")
+    } else { coletor.stop()
+        col.channel.send("Woops!")
+    }
+})
+coletor.on("end", e => data.message.channel.send("Acabou!"))
+}
+}
