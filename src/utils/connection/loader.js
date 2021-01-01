7a744a3b9f1a6e66f92c9aefc661e75d50aaf2c2
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 12:21:33 -0400

    Delete loader.js

--- src/utils/connection/loader.js
+++
@@ -1,6 +0,0 @@
-module.exports = function runLoaders(object, desposito) {
-    const Heroku = require("../../strc/loaders/heroku")
-    const heroku = new Heroku(object.heroku.port)
-    heroku.connect()
-}
-
