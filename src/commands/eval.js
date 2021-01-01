2aaef8d50ae00520641419287f9520fd27af3e5b
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 11:33:53 -0400

    Update eval.js

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,14 +1,12 @@
 const util = require('util')
 module.exports = {
     aliase: "e ev",
+    requireAcessPass: true,
 
-    async runHelp(data, desposito)  {
-        if(!["748320609746026607", "451920956768649226"].includes(data.message.author.id)) return
+    async open(data, desposito)  {
         let content = data.message.content
         const reg = /<@!?(\d{16,18})>/g
-
         content = content.replace(reg, 'desposito.users.cache.get("$1")')
-
         this.execute(desposito, data, content.split(" ").slice(2).join(" "))
     },
 
