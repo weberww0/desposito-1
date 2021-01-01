77d7259b232e52ea2d534d5fabebe15d904ab395
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 19:50:15 -0400

    Update math.js

--- src/commands/math.js
+++ src/commands/math.js
@@ -2,39 +2,37 @@ module.exports = {
     aliase: "tabuada m",
     async open(data, desposito) {
 
-    const defaultMessage = "> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\n"
-    let result
-    let correct = 0
-    let maths
+        let correct = 0
+        let maths
 
-    function regenerate() {
-        maths = {"one": Math.floor(Math.random() * (10-2) + 2), "two": Math.floor(Math.random() * (10-2) + 2)}
-        maths.result = maths.one * maths.two
-        runCollector()
-    }
+        function regenerate() {
+            maths = {"one": Math.floor(Math.random() * (10-2) + 2), "two": Math.floor(Math.random() * (10-2) + 2)}
+            maths.result = maths.one * maths.two
+            runCollector()
+        }
 
-    const filter = u => u.author.id === data.message.author.id
-    async function runCollector() {
-        const collector = data.message.channel.createMessageCollector(filter, {max: 1, time: 10000})
-        collector.on("collect", async (colected) => {
-            if(colected.content == maths.result) {
-                correct += 1
-                regenerate()
-                colected.delete()
-                message.edit(data.messages("math_congrats", maths))
-            } else {
-                message.edit(data.messages("math_fail", correct))
-                colected.delete()
-            }
-         })
-         collector.on("end", (col, reason) => {
-             if(reason === "time") {
-                 message.edit(data.messages("math_timeout", correct))
-             }
-         })
-    }
+        const filter = u => u.author.id === data.message.author.id
+        async function runCollector() {
+            const collector = data.message.channel.createMessageCollector(filter, {max: 1, time: 10000})
+            collector.on("collect", async (colected) => {
+                if(colected.content == maths.result) {
+                    correct += 1
+                    regenerate()
+                    colected.delete()
+                    message.edit(data.messages("math_congrats", maths))
+                } else {
+                    message.edit(data.messages("math_fail", correct))
+                    colected.delete()
+                }
+             })
+             collector.on("end", (col, reason) => {
+                 if(reason === "time") {
+                     message.edit(data.messages("math_timeout", correct))
+                 }
+             })
+        }
 
-    result = regenerate()
-    const message = await data.message.channel.send(data.messages("math_first", maths))
+        regenerate()
+        const message = await data.message.channel.send(data.messages("math_first", maths))
     }
 }
