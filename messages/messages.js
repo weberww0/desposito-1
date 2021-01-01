166f97a8307f7a9aa1f5bfa6d8cd32f4ec827d16
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 13:50:00 -0400

    Update messages.js

--- messages/messages.js
+++ messages/messages.js
@@ -1,1 +1,7 @@
-{}
+const messages = {"math_timeout": `Acabou o tempo! ${reference.one} and ${reference.two}`}
+
+module.exports = (ind, reference) => {
+    return messages[ind]
+}
+
+// msg ("math_timeout", {one: "a", two: "b"})
