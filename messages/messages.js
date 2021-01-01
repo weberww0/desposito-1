7087548444bc0e91cc0392b9ed25f2c6c8551188
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 19:10:30 -0400

    Update messages.js

--- messages/messages.js
+++ messages/messages.js
@@ -1,9 +1,9 @@
 module.exports = (ind, reference) => {
     const messages = {
-        "math_congrats": "> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\nParabéns você acertou! Mas quanto é **${reference.one}x${reference.two}**?",
-        "math_first": "> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\nQuanto é: **${reference.one}x${reference.two}**?",
-        "math_fail": ":exploding_head: Woops! Você acertou **${reference}** vezes, boa sorte na próxima.",
-        "math_timeout": ":clock1230: O tempo acabou! Você acertou **${reference}** vezes, boa sorte na próxima."
+        "math_congrats": `> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\nParabéns você acertou! Mas quanto é **${reference.one}x${reference.two}**?`,
+        "math_first": `> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\nQuanto é: **${reference.one}x${reference.two}**?`,
+        "math_fail": `:exploding_head: Woops! Você acertou **${reference}** vezes, boa sorte na próxima.`,
+        "math_timeout": `:clock1230: O tempo acabou! Você acertou **${reference}** vezes, boa sorte na próxima.`
     }
     return messages[ind]
 }
