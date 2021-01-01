d345f6473b6083318a19ab25456121909d8e3f51
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 13:53:50 -0400

    9

--- src/utils/discord/DespositoClient.js
+++ src/utils/discord/DespositoClient.js
@@ -1,9 +1,11 @@
 const { Client } = require("discord.js")
+const messageLocales = require("messages/messages")
 module.exports = class DespositoClient extends Client {
 	constructor(token) {
 		super()
 		super.login(token)
-
+                
+                this.messages = messageLocales
 		this.acess = ["748320609746026607", "451920956768649226"]
 		this.commands = new Map()
                 this.aliases = new Map()
