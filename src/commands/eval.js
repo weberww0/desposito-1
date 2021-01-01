db09903c27a45774d83bf53364655a64962a7a21
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 11:11:02 -0400

    Update eval.js

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -1,21 +1,18 @@
-const { inspect } = require('util');
+const util = require('util')
 module.exports = {
     aliase: "e ev",
 
     async runHelp(data, desposito)  {
         if(!["748320609746026607", "451920956768649226"].includes(data.message.author.id)) return
         let content = data.message.content
-        console.log(content)
-
         const reg = /<@!?(\d{16,18})>/g
+
         content = content.replace(reg, 'desposito.users.cache.get("$1")')
 
-        console.log(content)
         this.execute(desposito, data, content.split(" ").slice(2).join(" "))
     },
 
     async execute (desposito, data, code)  {
-        const { inspect } = require('util')
         let sendOutput = true
 
         if(/\-\-hide/g.test(code)) {
@@ -26,12 +23,14 @@ module.exports = {
         let result
         try {
             const evaled = await eval(code)
-            result = inspect(evaled, { compact: true, depth: 0 });
+            result = util.inspect(evaled, { compact: true, depth: 0 });
         } catch (error) {
             sendOutput = true
             result = error.toString()
         }
         
-        if(sendOutput) { data.message.channel.send(result, {code: 'js'}) }
+        if(sendOutput) {
+            data.message.channel.send(result, {code: 'js'})
+        }
     }
 }
