66f63b51a5e3467b5c853d924cf8a0a4cf806fe5
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 12:31:03 -0400

    Update math.js

--- src/commands/math.js
+++ src/commands/math.js
@@ -9,18 +9,17 @@ module.exports = {
 
     function regenerate() {
         maths = {"one": Math.floor(Math.random() * (10-2) + 2), "two": Math.floor(Math.random() * (10-2) + 2)}
-
+        maths.result = maths.one * maths.two
         runCollector()
-        return maths.one * maths.two
     }
 
     const filter = u => u.author.id === data.message.author.id
     async function runCollector() {
         const collector = data.message.channel.createMessageCollector(filter, {max: 1, time: 10000})
         collector.on("collect", async (colected) => {
-            if(colected.content == result) {
+            if(colected.content == maths.result) {
                 correct += 1
-                result = regenerate()
+                regenerate()
                 colected.delete()
                 message.edit(defaultMessage + "Parabéns você acertou! Mas quanto é **" + maths.one + "x" + maths.two + "**?")
             } else {
