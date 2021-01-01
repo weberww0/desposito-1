5fcf080af41c20bb1738cf6c80a7ae50bdc5a6a1
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 12:18:50 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,3 @@ const desposito = new DespositoClient(process.env.TOKEN)
 
 require("./strc/handlers/command")(desposito)
 require("./strc/handlers/event")(desposito)
-require("./utils/connection/loader")({heroku: {port: 5000}}, desposito)
