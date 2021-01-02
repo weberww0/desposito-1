59ecb64433a194ff919cc7d45a10b33c3def4aab
weberww0 <60146862+weberww0@users.noreply.github.com>
2021-01-01 20:16:02 -0400

    Update messages.js

--- messages/messages.js
+++ messages/messages.js
@@ -1,11 +1,13 @@
 module.exports = (ind, reference) => {
     const messages = {
-        "math_congrats": `> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\nParabéns você acertou! Mas quanto é **${reference.one}x${reference.two}**?`,
-        "math_first": `> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\nQuanto é: **${reference.one}x${reference.two}**?`,
-        "math_fail": `:exploding_head: Woops! Você acertou **${reference}** vezes, boa sorte na próxima.`,
-        "math_timeout": `:clock1230: O tempo acabou! Você acertou **${reference}** vezes, boa sorte na próxima.`
+        math: {
+            "congrats": `> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\nParabéns você acertou! Mas quanto é **${reference.one}x${reference.two}**?`,
+            "first": `> 🥸 Tabuada!\n> Acerte o máximo possível e ganhe absolutamente nada.\n\nQuanto é: **${reference.one}x${reference.two}**?`,
+            "fail": `:exploding_head: Woops! Você acertou **${reference}** vezes, boa sorte na próxima.`,
+            "timeout": `:clock1230: O tempo acabou! Você acertou **${reference}** vezes, boa sorte na próxima.`
+        }
     }
-    return messages[ind]
-}
 
-// msg ("math_timeout", {one: "a", two: "b"})
+    const propArray = ind.split(".")
+    return messages[propArray[0]][propArray[1]]
+}
