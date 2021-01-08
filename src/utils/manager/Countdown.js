465af2ab5e39b4473c7345b265934d20dbdc15f9
trywesley <66664225+trywesley@users.noreply.github.com>
2021-01-08 17:45:35 -0400

    Update Countdown.js

--- src/utils/manager/Countdown.js
+++ src/utils/manager/Countdown.js
@@ -6,7 +6,7 @@ module.exports = class CountdownManager {
         const user = message.author
 
         if(cownLection.has(user.id)) {
-            const calc = cownLection.get(user.id).time - Math.floor((Date.now() - cownLection.get(user.id)).Date / 1000)
+            const calc = cownLection.get(user.id).time - Math.floor((Date.now() - cownLection.get(user.id).Date) / 1000)
             return message.channel.send("Countdown: " + calc + " segundo(s)")
         } else {
             cownLection.set(user.id, { time: time, Date: Date.now() })
