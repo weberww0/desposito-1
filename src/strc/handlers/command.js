0e1fe750df6d90991b846e9b4ceb4c33f58d5aec
trywesley <66664225+trywesley@users.noreply.github.com>
2021-01-26 14:04:59 -0400

    Update command.js

--- src/strc/handlers/command.js
+++ src/strc/handlers/command.js
@@ -2,16 +2,17 @@ const fs = require("fs")
 module.exports = (desposito) => {
     fs.readdirSync('src/commands').forEach(f => {
         const command = require(`../../commands/${f}`)
+        const commandName = f.replace(/.js/g, "")
     
         if(command.aliase) {
             const aliases = command.aliase.split(" ")
             aliases.forEach(aliase => {
                 console.log("Command", "Aliase " + aliase + " adicionada")
-                desposito.aliases.set(aliase, command)
+                desposito.aliases.set(aliase, commandName)
             })
         }
     
-        desposito.commands.set(f.replace(/.js/g, ""), command)
+        desposito.commands.set(commandName, command)
         console.log("Command", "Carregamento do comando " + f + " concluido\n")
     })
 }
